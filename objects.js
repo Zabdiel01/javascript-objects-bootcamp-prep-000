f4685f15eb556e276db0ae62a646e3ea296da831
Zabdiel Ramos <zabdiel.ramos01@gmail.com>
2018-02-16 06:08:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,7 @@ function updatePlaylist(playlist, artist, song) {
 }
 function removeFromPlaylist(playlist,artist) {
   
-  delete playlist.artist
-  return playlist
+  delete playlist.artist;
+  var newPlaylist = {};
+  return newPlaylist;
 }
