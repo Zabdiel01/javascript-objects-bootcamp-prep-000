d8b1bef227e46f736cceb422e6e0a7c4237c0d39
Zabdiel Ramos <zabdiel.ramos01@gmail.com>
2018-02-16 06:03:25 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {"A7X":"Brompton Cocktail"};
+function updatePlaylist(playlist, artist, song) {
+  return Object.assign(playlist, {[artist]:song})
+}
+function removeFromPlaylist(playlist,artist) {
+  
+  delete playlist.artist
+  return playlist
+}
